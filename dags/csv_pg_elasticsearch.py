941a341dfd981c4ab8748cdf6055e07752543fdc
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-11-29 03:18:09 +0530

    add branchpythonoperator

--- dags/csv_pg_elasticsearch.py
+++ dags/csv_pg_elasticsearch.py
@@ -1,9 +1,11 @@
 from airflow import DAG
 
 from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.utils.trigger_rule import TriggerRule
+
 from datetime import datetime
 import psycopg2
 
@@ -16,12 +18,19 @@ dag = DAG(
 
 with dag:
     # There will be 4 tasks,
-    # 1 -> Create table in postgres.
-    # 2 -> Wait for the file using the FileSensor
+    # 1 -> Check if the table already exists.
+    # 2 -> Create table in postgres OR Wait for the file using the FileSensor (If the table is already there, I am using BranchPythonOperator for that.)
     # 3 -> Once the file arrives, push the data into postgreSQL
     # 4 -> From postgres, data should go to ElasticSearch via logstash (Plan is to use bash operator)
 
-    # TASK 1 -> Create table in postgres. (//TODO: CHECK IF THE TABLE ALREADY EXISTS)
+    def check_table():
+        return ["sense_the_csv", "Sensed_CSV_to_Postgres", "Postgres_to_ElasticSearch"]
+
+    check_for_table = BranchPythonOperator(
+        task_id="Check_for_table", python_callable=check_table,
+    )
+
+    # TASK 2 (Case if the table is not already there.) -> Create table in postgres.
     create_table = PostgresOperator(
         task_id="Create_Table_in_Postgres",
         sql="""CREATE TABLE sampletable(
@@ -34,8 +43,9 @@ with dag:
         """,
     )
 
-    # TASK 2 -> Wait for the file using the FileSensor
+    # TASK 2 (If the table is there, then this will be the second task) -> Wait for the file using the FileSensor
     file_sensing_task = FileSensor(
+        trigger_rule=TriggerRule.ONE_SUCCESS,
         task_id="sense_the_csv",
         filepath="sample1.csv",
         fs_conn_id="my_file_system",
@@ -63,4 +73,5 @@ with dag:
         bash_command="/Users/yudi/ELK/logstash-7.10.0/bin/logstash -f /Users/yudi/ELK/logstash-7.10.0/bin/logstash-simple.conf",
     )
 
-    create_table >> file_sensing_task >> push_data_to_pg >> push_data_to_elastic
+    check_for_table >> create_table >> file_sensing_task >> push_data_to_pg >> push_data_to_elastic
+    check_for_table >> file_sensing_task >> push_data_to_pg >> push_data_to_elastic
