b2371613b2e51eede2d4798c123df3153e97a42a
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-11-30 03:51:55 +0530

    write a script to stream tweets and make Tweets class

--- dags/tweets_to_elastic.py
+++ dags/tweets_to_elastic.py
@@ -16,6 +16,38 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
-api.update_status(
-    "From now on, some my tweets will be from a python script. Like this one!"
-)
+
+class Tweet:
+    def __init__(self, message, name, screen_name, id_string, location):
+        self.message = message
+        self.name = name
+        self.screen_name = screen_name
+        self.id_string = id_string
+        self.location = location
+
+
+class MyStreamListener(tw.StreamListener):
+    def on_status(self, status):
+        new_tweet = Tweet(
+            status.text,
+            status.user.name,
+            status.user.screen_name,
+            status.user.id_str,
+            status.user.location,
+        )
+        print(new_tweet)
+        print(status.user.name)
+        print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+
+myStreamListener = MyStreamListener()
+myStream = tw.Stream(auth=api.auth, listener=myStreamListener)
+
+myStream.filter(track=["water"], is_async=True)
+
+
+# text, user {'name', 'id_str', 'location', 'screen_name', }
