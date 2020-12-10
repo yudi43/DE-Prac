4b3876377904a5c92fe7c0eeb45f8bec1f96f28a
Yudhishthir Singh <yudi@Yudhishthirs-MacBook-Pro.local>
2020-12-11 03:25:24 +0530

    Initial code for new pipeline - PG backup to blob storage

---
+++ dags/pg_backup_to_blob_storage.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from datetime import datetime
+
+with DAG(
+    "Send PG backup to Azure Data Blob",
+    description="This pipeline is made for taking backup of postgres database and seding to Azure data blob in tar.gz format",
+    schedule_interval="*/5 * * * *",
+    start_date=datetime(2019, 11, 1),
+    catchup=False
+) as dag:
+    print("do something")
+
+    # SPLITTING THE TASKS:
+
+    # TASK 1: Take backup of a database on local storage.
+    # TASK 2: Upload it to azure data blob
