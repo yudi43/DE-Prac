78600ec8fa855048945369af448242848a279fc1
Yudi <singh.yudi10@gmail.com>
2020-12-30 11:02:13 +0530

    airflow with parameters from cli

--- dags/control_with_terminal.py
+++ dags/control_with_terminal.py
@@ -1,8 +1,9 @@
 from airflow import DAG
-#import operators...
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 import psycopg2
+from psycopg2 import sql
 
 
 with DAG(
@@ -12,12 +13,29 @@ with DAG(
     start_date=datetime(2018,11,1),
     catchup=False,
 ) as dag:
-    def my_func():
-        print("This is a test..")
+    def create_schema(ds, **kwargs):
+        print("Remotely received value of {} for key = message".format(kwargs['dag_run'].conf['client_name']))
+        try:
+            conn = psycopg2.connect(database="testdb", host="localhost")
+            cursor = conn.cursor()
+            schema_name = kwargs['dag_run'].conf['client_name']
+            cursor.execute(
+                sql.SQL("create schema {}")
+                .format(sql.Identifier(schema_name)))
+            conn.commit()
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
     
     test_task = PythonOperator(
         task_id="test_task",
-        python_callable=my_func
+        python_callable=create_schema,
+        provide_context = True
     )
+
+    # bash_task = BashOperator(
+    #     task_id = "bash-task",
+    #     bash_command='echo "Here is the message: '
+    #              '{{ dag_run.conf["message"] if dag_run else "" }}" ',
+    # )
 
     test_task
